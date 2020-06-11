f2b630f9c99f34105960afdb380866f362f80e46
Carsten Stecker <stecker.carsten10@gmail.com>
2020-06-11 23:32:06 +0200

    added login

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import json
 
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
 from flask_peewee.db import Database
 
 app = Flask(__name__)
@@ -22,6 +22,21 @@ def goodbye(u: int):
     print(u)
     return render_template('number.html', number=u)
 
+
+# TODO: sessioncookie
+@app.route('/login', methods=['POST'])
+def POST_login():
+    username = request.form.get('username')
+    password = request.form.get('password')
+    redirect_url = request.form.get('redirect')
+    if username == "total" and password == "sicher":
+        return redirect(redirect_url, code=302)
+    else:
+        return "Invalid Username"
+
+@app.route('/login', methods=['GET'])
+def GET_login():
+    return """Hallo Welt"""
 
 # endregion a
 

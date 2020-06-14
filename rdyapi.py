7135f80544942f08b5b6d066d4b9269b09e4a0d4
Roman Gräf <romangraef@loves.dicksinhisan.us>
2020-06-14 21:33:54 +0200

    api
    Signed-off-by: Roman Gräf <romangraef@loves.dicksinhisan.us>

---
+++ rdyapi.py
@@ -0,0 +1,32 @@
+import requests
+
+
+class RdyApi:
+
+    def __init__(self, hostname: str, api_key: str):
+        self.hostname = hostname
+        self.session = requests.Session()
+        self.session.headers.update(dict(
+            session=api_key
+        ))
+
+    def create_user(self, username: str, password: str, group_id: str = "nogroup"):
+        self.session.post(f'https://{self.hostname}/admin/users',
+                      json=dict(
+                          userName=username,
+                          password=password,
+                          groupId=group_id,
+                          role='LOGGED_IN',  # hier könnte ihre rolle stehen
+                      )).raise_for_status()
+
+    def create_group(self, group_id: str, group_name: str):
+        self.session.post(f'https://{self.hostname}/admin/groups',
+                      json=dict(
+                          groupId=group_id,
+                          groupName=group_name,
+                      )).raise_for_status()
+
+    def get_users(self):
+        res = self.session.get(f'https://{self.hostname}/admin/users')
+        res.raise_for_status()
+        return res.json()
